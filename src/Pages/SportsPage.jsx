3a15f500d39e70497dde9b1771fcecaa83dd533d
Mariastakh <astakhovam94@gmail.com>
2020-05-14 16:14:18 +0100

    show the losing teams list

--- src/Pages/SportsPage.jsx
+++ src/Pages/SportsPage.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class SporstPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { team: "" };
+    this.state = { team: "", losingTeams: [] };
   }
 
   handleTeamChange = (event) => {
@@ -13,7 +14,37 @@ export default class SporstPage extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     this.props.handleTeam(this.state.team);
+
+    axios
+      .post("http://localhost:8000/sport", {
+        winningTeam: this.state.team,
+      })
+      .then((response) => {
+        // handle success
+        this.setState({ losingTeams: response.data.losingTeams });
+        console.log(this.state.losingTeams);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
   };
+
+  Teams(options) {
+    // run the tests agains every element in the array
+    console.log("the teams: ", options.losingTeams.length);
+    if (options.losingTeams.length === 0) {
+      return <></>;
+    } else {
+      const teamList = options.losingTeams.map((team) => (
+        <li key={team}>{team}</li>
+      ));
+      return <ul>{teamList}</ul>;
+    }
+  }
 
   render() {
     const { team } = this.state;
@@ -33,6 +64,7 @@ export default class SporstPage extends Component {
             />
           </div>
         </form>
+        <this.Teams losingTeams={this.state.losingTeams} />
       </>
     );
   }

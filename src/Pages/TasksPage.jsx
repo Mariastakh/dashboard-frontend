f4f21f36ec520ef07848425149583010d8ed6273
Mariastakh <astakhovam94@gmail.com>
2020-05-16 15:44:20 +0100

    get task input field update working

--- src/Pages/TasksPage.jsx
+++ src/Pages/TasksPage.jsx
@@ -6,22 +6,44 @@ export default class TasksPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { tasks: "" };
+    this.state = {
+      tasks: [
+        { name: "wash up", status: false, id: 13 },
+        { name: "get up on time", status: true, id: 12 },
+      ],
+    };
+
     this.handleTask = this.handleTask.bind(this);
   }
 
   handleTask(data) {
-    console.log(data);
+    // change tasks:
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.map((el, index) =>
+        index === data[3] ? { ...el, name: data[0] } : el
+      ),
+    }));
+  }
+
+  componentDidUpdate() {
+    // CHECK IF COMPONENT UPDATED AND CALL API
+    // to avoid an infinite loop, the API call needs to be inside a conditional statement.
+    console.log("END STATE: ", this.state.tasks);
   }
 
   Tasks(options) {
-    const tasks = ["task one", "task two"];
+    //console.log("tasks inside options: ", options.tasks[0]);
+    // const tasks = [
+    //   { name: "wash up", status: false, id: 13 },
+    //   { name: "get up on time", status: true, id: 12 },
+    // ];
+    console.log("OPTONS: ", options.tasks[0].name);
 
-    if (tasks.length === 0) {
+    if (options.tasks.length === 0) {
       return <>You have no tasks</>;
     } else {
-      const taskList = tasks.map((task) => (
-        <Task task={task} handleTask={options.handleTask} />
+      const taskList = options.tasks.map((task, index) => (
+        <Task task={task} id={index} handleTask={options.handleTask} />
       ));
       return <ul>{taskList}</ul>;
     }
@@ -31,7 +53,7 @@ export default class TasksPage extends Component {
     axios
       .get("http://localhost:8000/tasks")
       .then((response) => {
-        this.setState({ tasks: response.data.tasks });
+        // this.setState({ tasks: response.data.tasks });
       })
       .catch(function (error) {
         console.log(error);

8d9f4b7a9585ad3ceae67b4f2a2917f5193553b7
Mariastakh <astakhovam94@gmail.com>
2020-05-18 15:06:09 +0100

    resize s3 image

--- src/Components/PhotoPreview.js
+++ src/Components/PhotoPreview.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import styles from "./Preview.module.css";
-import container from "../assets/Container.png";
 
 export default class PhotoPreview extends Component {
   constructor(props) {
@@ -11,17 +10,13 @@ export default class PhotoPreview extends Component {
     return (
       <a style={{ textDecoration: "none", color: "black" }} href="/photos">
         <div className={styles.preview} data-testid="photopreview">
-          Photos
-          <br></br>
-      
+          <p className={styles.title}>Photos</p>
+          <div classname={styles.previewContent}>
             <img
-              className={styles.img}
-              src={container}
-              alt="your photo collection"
+              style={{ width: 100 }}
+              src="https://dashboard-images-bucket.s3.eu-west-2.amazonaws.com/t1"
             />
-            <img src="https://dashboard-images-bucket.s3.eu-west-2.amazonaws.com/t1" />
-      
-        <p className={styles.title}>Photos</p>
+          </div>
         </div>
       </a>
     );

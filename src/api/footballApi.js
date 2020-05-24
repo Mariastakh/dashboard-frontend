50980a61814052dba3d15b3fbe5617cc4867de34
Mariastakh <astakhovam94@gmail.com>
2020-05-24 16:51:00 +0100

    add parameters to teams url call

--- src/api/footballApi.js
+++ src/api/footballApi.js
@@ -5,5 +5,12 @@ const baseUrl = process.env.REACT_APP_API_URL + "/teams/";
 export function getTeams(team) {
   // axios.get(baseUrl).then(handleResponse).catch(handleError);
 
-  return axios.get(baseUrl).then(handleResponse).catch(handleError);
+  return axios
+    .get(baseUrl, {
+      params: {
+        team: team,
+      },
+    })
+    .then(handleResponse)
+    .catch(handleError);
 }

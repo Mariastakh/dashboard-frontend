2b609ae800effd1c27d1ae94e5e37703e27b2eb0
Mariastakh <astakhovam94@gmail.com>
2020-05-26 10:35:33 +0100

    extract all business logic from Login, rename to LoginForm

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,70 +1,40 @@
-import React, { Component } from "react";
-import { getUser } from "../api/getUser";
+import React from "react";
 
-function Login() {
+function Login(props) {
+  return (
+    <>
+      <form onSubmit={props.onSubmit}>
+        <div data-testid="username">
+          <label>Username</label>
+          <input
+            data-testid="usernameinput"
+            name="username"
+            type="text"
+            value={props.user.username}
+            onChange={props.onChange}
+            required
+          />
+        </div>
 
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleUsernameChange = (event) => {
-    this.setState({ username: event.target.value });
-  };
-
-  handlePasswordChange = (event) => {
-    this.setState({ password: event.target.value });
-  };
-
-  //"https://em7jsvk2ig.execute-api.eu-west-2.amazonaws.com/production/"
-  // "http://localhost:8000/"
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    getUser(this.state.username, this.state.password).then((response) => {
-      console.log("response is: ", response);
-      localStorage.setItem("jwt", response.data.token);
-      this.props.handleSuccessfulAuth(response.data);
-    });
-  };
-
-
-  
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <div data-testid="username">
-            <label>Username</label>
-            <input
-              data-testid="usernameinput"
-              name="username"
-              type="text"
-              value={username}
-              onChange={this.handleUsernameChange}
-              required
-            />
-          </div>
-
-          <div data-testid="password">
-            <label>Password</label>
-            <input
-              data-testid="passwordinput"
-              name="password"
-              type="password"
-              value={password}
-              onChange={this.handlePasswordChange}
-              required
-            />
-          </div>
-          <div data-testid="submit">
-            <button type="submit" className="button">
-              Submit
-            </button>
-          </div>
-        </form>
-      </>
-    );
+        <div data-testid="password">
+          <label>Password</label>
+          <input
+            data-testid="passwordinput"
+            name="password"
+            type="password"
+            value={props.user.password}
+            onChange={props.onChange}
+            required
+          />
+        </div>
+        <div data-testid="submit">
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
-
 
 export default Login;
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import Login from "../Components/Login";
-import getUser from "../api/getUser";
+import { getUser } from "../api/getUser";
 
 function LoginPage(props) {
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
 
-  function handleChange(event) {}
+  function handleChange({ target }) {
+    setUser({
+      ...user,
+      [target.name]: target.value,
+    });
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     getUser(user.username, user.password).then((response) => {
+      setUser(user);
       console.log("response is: ", response);
       localStorage.setItem("jwt", response.data.token);
-      localStorage.setItem("user", data.user.username);
+      localStorage.setItem("user", user.username);
       props.history.push("/dashboard");
     });
   }
